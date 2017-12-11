d504b42b965275c86ce54900dfde9a169697681d
carlos.oliveira <carlos.oliveira@altran-ais.com.br>
2017-12-11 08:44:11 -0200

    scaffold inicial

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+module.exports = function(env){
+    const nodeEnv = env && env.prod ? 'production' : 'development';
+    const isProd = nodeEnv === 'production';
+    
+    return {
+        entry: isProd ? [
+            './src/index.js'
+            ] : [
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+            './src/index.js'
+        ],
+        devtool :"cheap-module-source-map",
+        module: {
+            rules: [{
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'react-hot-loader',
+                        'babel-loader'
+                    ].slice( isProd ? 1 : 0)
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'eslint-loader']
+                },{
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: ['css-loader', 'sass-loader'],
+                        fallback: "style-loader"
+                    })
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.css'],
+        },
+        output: {
+            path: __dirname + '/dist',
+            publicPath: '/',
+            filename: 'bundle.js'
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+                }
+            })
+        ].concat( isProd ? [
+            new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+            new ExtractTextPlugin({
+                filename: 'bundle.css',
+                disable: false,
+                allChunks: true
+            }),
+            new webpack.optimize.AggressiveMergingPlugin({
+                minSizeReduce: 1,
+                moveToParents: true
+    
+            })
+            ] : [
+                new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+                new ExtractTextPlugin({ disable: true })
+            ]
+        ),
+        devServer: {
+            contentBase: './dist',
+            historyApiFallback: true
+        }
+    
+    }
+}
