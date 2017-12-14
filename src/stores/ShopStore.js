0dde75714a8e8ea97e48d4bcedd01dc384f226f8
carlos.oliveira <carlos.oliveira@altran-ais.com.br>
2017-12-14 00:19:34 -0200

    Add: Mock data e Store das lojas

---
+++ src/stores/ShopStore.js
@@ -0,0 +1,25 @@
+import { observable, computed } from "mobx";
+import { stores } from '../mock/data.json';
+
+export default class ShopStore{
+    @observable _shops = stores
+    @observable currentPage = 1
+    @observable filter = ''
+    @observable itensPerPage = 10
+
+    doFilter = store => store.name.indexOf(this.filter) != -1
+    setPage = page =>  this.currentPage = page
+
+    @computed get pages() {
+        return Math.ceil( this._shops.filter(this.doFilter).length / this.itensPerPage )
+    }
+    @computed get range(){
+        return [(this.currentPage - 1) * this.itensPerPage , this.currentPage * this.itensPerPage]
+    }
+    
+    get shops(){
+        return this._shops
+            .filter(this.doFilter)
+            .slice( ...this.range )
+    }
+}
